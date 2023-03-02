c0a44f18477cc89529bc36388a88a26151ab536f
amirparvaneh <amirparvaneh138@gmail.com>
2023-03-02 22:00:23 +0330

    algorithm-1

--- src/algorithm/Salinpo.java
+++ src/algorithm/Salinpo.java
@@ -4,43 +4,49 @@ import java.util.*;
 
 public class Salinpo {
     static final int MOD = 1_000_000_007;
-    static int[] a;
-    static int[] memo;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        a = new int[n + 1];
-        memo = new int[1 << n];
-
+        int[] a = new int[n + 1];
         for (int i = 1; i <= n; i++) {
             a[i] = scanner.nextInt();
         }
-
-        int result = countWays(0, n, 0);
-
-        System.out.println(result);
-    }
-
-    static int countWays(int used, int n, int player) {
-        if (used == (1 << n) - 1) {
-            return 1;
-        }
-        if (player > n) {
-            return 0;
-        }
-        if (memo[used] != 0) {
-            return memo[used];
-        }
-        int ways = 0;
-
+        int[] count = new int[n + 1];
         for (int i = 1; i <= n; i++) {
-            if (a[i] == 0 && ((used >> (i - 1)) & 1) == 0 && i != player) {
-                ways = (ways + countWays(used | (1 << (i - 1)), n, player + 1)) % MOD;
+            if (a[i] != 0) {
+                count[a[i]]++;
             }
         }
+        long ans = 1;
+        boolean[] vis = new boolean[n + 1];
+        for (int i = 1; i <= n; i++) {
+            if (!vis[i] && a[i] == 0) {
+                int cnt = 0;
+                int j = i;
+                while (!vis[j] && a[j] == 0) {
+                    vis[j] = true;
+                    cnt++;
+                    j = a[j] == 0 ? j + 1 : a[j];
+                }
+                if (a[j] != 0) {
+                    count[a[j]] -= cnt;
+                    if (count[a[j]] < 0) {
+                        System.out.println(0);
+                        return;
+                    }
+                }
+                ans = (ans * countWays(cnt)) % MOD;
+            }
+        }
+        System.out.println(ans);
+    }
 
-        memo[used] = ways;
-        return ways;
+    static long countWays(int n) {
+        long ans = 1;
+        for (int i = 2; i <= n; i++) {
+            ans = (ans * i) % MOD;
+        }
+        return ans;
     }
 }
