f8edaaedf4f389cca443d29c5865b3d4108d5237
amirparvaneh <amirparvaneh138@gmail.com>
2023-03-02 18:23:11 +0330

    algorithm-1

--- src/algorithm/LaCrema.java
+++ src/algorithm/LaCrema.java
@@ -10,18 +10,25 @@ public class LaCrema {
         for (int i = 0; i < n; i++) {
             b[i] = sc.nextInt();
         }
+
+        // Calculate cumulative time required to burn matches from the beginning
+        double[] cumTime = new double[n];
+        cumTime[0] = b[0];
+        for (int i = 1; i < n; i++) {
+            cumTime[i] = cumTime[i-1] + b[i];
+        }
+
         int q = sc.nextInt();
         while (q-- > 0) {
             int l = sc.nextInt();
             int r = sc.nextInt();
-            double time = 0;
+            double time = cumTime[r] - cumTime[l] + b[l];
+
+            // Adjust time for matches lit from both ends
             for (int i = l; i < r; i++) {
-                time += (double)b[i] / 2;
-            }
-            if (l != r) {
-                time += (double)(b[l] + b[r]) / 4;
-            } else {
-                time += (double)b[l] / 2;
+                if (b[i] % 2 == 0) {
+                    time -= (double)b[i]/2;
+                }
             }
             System.out.printf("%.1f\n", time);
         }
