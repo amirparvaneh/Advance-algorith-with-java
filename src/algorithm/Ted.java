4b7646bbcc7b40d58cd48e628a1d846caf2734ab
amirparvaneh <amirparvaneh138@gmail.com>
2023-03-03 08:54:32 +0330

    algorithm-1

--- src/algorithm/Ted.java
+++ src/algorithm/Ted.java
@@ -1,26 +1,43 @@
 package algorithm;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class Ted {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-        int[] tickets = new int[n];
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
         for (int i = 0; i < n; i++) {
-            tickets[i] = input.nextInt();
+            a[i] = sc.nextInt();
         }
         int[] seats = new int[n];
         for (int i = 0; i < n; i++) {
-            int x = tickets[i];
-            if (seats[x-1] == 0) {
-                seats[x-1] = i+1;
+            int x = a[i];
+            if (seats[x - 1] == 0) {
+                seats[x - 1] = i + 1;
             } else {
                 int j = x;
-                while (j < n && seats[j-1] != 0) {
+                while (j <= n && seats[j - 1] != 0) {
                     j++;
                 }
-                seats[j-1] = i+1;
+                if (j <= n && seats[j - 1] == 0) {
+                    seats[j - 1] = i + 1;
+                } else {
+                    j = 1;
+                    while (j < x && seats[j - 1] != 0) {
+                        j++;
+                    }
+                    if (j < x && seats[j - 1] == 0) {
+                        seats[j - 1] = i + 1;
+                    } else {
+                        for (int k = 0; k < n; k++) {
+                            if (seats[k] == 0) {
+                                seats[k] = i + 1;
+                                break;
+                            }
+                        }
+                    }
+                }
             }
         }
         for (int i = 0; i < n; i++) {
